e231adc5a7113e4f2c4712716370743b55987955
Sem van den Berge <sem.van.den.berge@student.uva.nl>
2026-06-03 16:57:14 +0200

    added isDeleted to user entity + checking if the gitignore works

--- Backend/src/Domain/Entities/AppUser.cs
+++ Backend/src/Domain/Entities/AppUser.cs
@@ -18,6 +18,9 @@ public class AppUser
     public string? VerificationToken { get; set; }
     public DateTime? VerificationTokenExpiry { get; set; }
 
+    // soft delete -> we keep them but anonymize them and prevent login (so we don't lose game history, but they can't log in anymore)
+    public Boolean IsDeleted { get; set; } = false;
+
     // stats
     public int Wins { get; set; }
     public int Losses { get; set; }
