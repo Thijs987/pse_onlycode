d0bff18fc08a005ade335606a13e483b8df6a926
Joffry <joffryturbo@gmail.com>
2026-06-03 03:59:00 +0200

    Card pile + count script added

---
+++ frontend/pile_click.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+#Deze aanpassen voor het goeie aantal kaarten
+@export var card_count: int = 40
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.input_event.connect(_on_pile_input_event)
+
+func _on_pile_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	#Check voor muisklik en of knop is ingedrukt
+	if event is InputEventMouseButton and event.pressed:
+		decrease_counter()
+
+# Haalt 1 kaart van de counter
+func decrease_counter():
+	if card_count > 0:
+		card_count -= 1
+	else:
+		print("Pile is empty")
