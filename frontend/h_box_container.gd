bb0a6690cc69769dda51f7d19ecc91a3f20e95f4
15838234 <kevin.bergsma@student.uva.nl>
2026-06-03 21:02:57 +0200

    multiple cards
    Er kunnen nu meerdere kaarten onstaan door op een knop te klikken

--- frontend/h_box_container.gd
+++ frontend/h_box_container.gd
@@ -1,14 +1,9 @@
 extends HBoxContainer
 
-@onready var Card: PackedScene = preload("res://card.tscn") 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var Card1 = Card.instantiate()
-	add_child(Card1)
-
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
