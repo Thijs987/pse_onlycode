963764b1a8eab277bb944fe94e811815f2d4b4af
MeesvT <mees.van.tol3@student.uva.nl>
2026-06-03 23:43:12 +0200

    Improved Godot-Server websocket connection

--- frontend/GSCWS.gd
+++ frontend/GSCWS.gd
@@ -1,24 +1,27 @@
 extends Node
 
+signal card_played(player_id, card_id)
+signal card_drawn(player_id, card_count)
+
 var socket := WebSocketPeer.new()
 
-func _Ready():
+# Connect the socket to the server
+func _ready():
 	var err = socket.connect_to_url("ws://localhost:5025/lobby")
 
 	if err != OK:
 		print("Failed to connect:", err)
 
 
-func _Process(_delta):
+# Updates the websocket and checks for incoming messages
+func _process(_delta):
 	socket.poll()
 
-	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
-		_Send({
-			"type": "TEST"
-		})
-		
-		set_process(false)
-		
+	while socket.get_available_packet_count() > 0:
+		var packet = socket.get_packet()
+		var text = packet.get_string_from_utf8()
+
+		_handle_message(text)
 
 
 # Function to play a card
@@ -36,6 +39,36 @@ func Draw_Card():
 	})
 
 
+# Function to start match
+func Start_Match():
+	_Send({
+		"type": "START_MATCH"
+	})
+	
+
+
 # Helper function to send data
 func _Send(data: Dictionary):
+	if socket.get_ready_state() != WebSocketPeer.STATE_OPEN:
+		print("Socket is not connected.")
+		return
+
 	socket.send_text(JSON.stringify(data))
+
+
+# Interprets the data sent by the server
+func _handle_message(text: String):
+	var data = JSON.parse_string(text)
+
+	match data["type"]:
+		"CARD_PLAYED":
+			card_played.emit(
+				data["player_id"],
+				data["card_id"]
+			)
+
+		"CARD_DRAWN":
+			card_drawn.emit(
+				data["player_id"],
+				data["card_count"]
+			)
