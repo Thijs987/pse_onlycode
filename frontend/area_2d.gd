da6504400eb2a27fb6cc311033cff40ac04b217b
Noah <Noah.Eggink@student.uva.nl>
2026-06-05 01:26:40 +0200

    Reapply "Merge branch 'controller' into GTS-parsingdata"
    This reverts commit b7cc97c7eceda10e546f327d34ea69f731ed7604.

---
+++ frontend/area_2d.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+# Deze aanpassen voor het goeie aantal kaarten
+@export var card_count: int = 40
+
+# Hier kan dan de echte pile voor de database geimporteerd worden
+# (Import line)
+
+# Maakt CounterLabel node
+@onready var counter_label: Label = $CounterLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.input_event.connect(_on_pile_input_event)
+	update_card_text()
+
+func _on_pile_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	# Check voor muisklik en of knop is ingedrukt
+	if event is InputEventMouseButton and event.pressed:
+		# Laat card count getal zien
+		decrease_counter()
+
+# Haalt 1 kaart van de counter
+func decrease_counter():
+	if card_count > 0:
+		card_count -= 1
+		# Past card count getal aan
+		update_card_text()
+	else:
+		print("Pile is empty")
+
+# Past card count getal aan
+func update_card_text():
+	counter_label.text = str(card_count)
+
+# Hier moet een functie die de volgende kaart uit de string/array pakt
+# func ...
---
+++ frontend/player_hand.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+const CARD_COUNT = 5
+const CARD_SCENE_PATH = "res://card.tscn"
+const CARD_WIDTH = 120
+const HAND_Y = 500
+
+var player_hand = []
+var center_screen_x
+
+@export var hand_curve = Curve
+@export var rotation_curve = Curve
+
+@export var max_rotation := 5
+@export var separation := -10
+@export var y_min := 0
+@export var y_max := -15
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	center_screen_x = get_viewport().size.x / 2
+	var card_scene = preload(CARD_SCENE_PATH)
+	for i in range(CARD_COUNT):
+		var new_card = card_scene.instantiate()
+		$"../CardLogic".add_child(new_card)
+		new_card.name = "Card"
+		add_card_to_hand(new_card)
+
+func add_card_to_hand(card):
+	if card not in player_hand:
+		player_hand.insert(0, card)
+		update_card_hand_position()
+	else:
+		move_to_position(card, card.hand_position)
+
+func update_card_hand_position():
+	for i in range(player_hand.size()):
+		var new_position = Vector2(calculate_card_position(i), HAND_Y)
+		var moving_card = player_hand[i]
+		moving_card.hand_position = new_position
+		move_to_position(moving_card, new_position)
+		
+
+func calculate_card_position(position):
+	var total_width = (player_hand.size() - 1) * CARD_WIDTH
+	var x_offset = center_screen_x + position * CARD_WIDTH - total_width / 2
+	return x_offset
+
+func move_to_position(card, position):
+	var tween = get_tree().create_tween()
+	tween.tween_property(card, "position", position, 0.2)
