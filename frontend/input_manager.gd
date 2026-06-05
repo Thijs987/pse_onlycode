e48a087df0620efa7cafe2d21368ba4bc3266ea6
Thijs Burger <thijsburger2006@gmail.com>
2026-06-05 11:17:48 +0200

    Put the Pile logic in the pile node

--- frontend/input_manager.gd
+++ frontend/input_manager.gd
@@ -12,17 +12,18 @@ var pile_reference
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	card_logic_reference = $"../CardLogic"
+	pile_reference = $"../Pile"
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
-			var mask_id = check_at_cursor()
+			var mask_id = check_at_cursor(true)
 			emit_signal("left_mouse_pressed")
 		else:
-			var mask_id = check_at_cursor()
+			var mask_id = check_at_cursor(false)
 			emit_signal("left_mouse_release")
 			
-func check_at_cursor():
+func check_at_cursor(is_pressed):
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
 	parameters.position = get_global_mouse_position()
@@ -36,7 +37,8 @@ func check_at_cursor():
 			if card_found and card_found.movable:
 				card_logic_reference.start_dragging(card_found)
 		elif result_collision_mask == COLLISION_MASK_PILE:
-			pass
+			if is_pressed:
+				pile_reference.decrease_counter()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ frontend/pile.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+# Deze aanpassen voor het goeie aantal kaarten
+@export var card_count: int = 40
+
+# Hier kan dan de echte pile voor de database geimporteerd worden
+# (Import line)
+
+# Maakt CounterLabel node
+@onready var counter_label: Label = $Pile_area/CounterLabel
+
+var hand_reference
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hand_reference = $"../PlayerHand"
+	#$"../InputManager".connect("left_mouse_pressed", on_pile_input_event)
+	update_card_text()
+
+# Haalt 1 kaart van de counter
+func decrease_counter():
+	if card_count > 0:
+		card_count -= 1
+		# Past card count getal aan
+		update_card_text()
+		hand_reference.new_card()
+	else:
+		print("Pile is empty")
+
+# Past card count getal aan
+func update_card_text():
+	counter_label.text = str(card_count)
+
+# Hier moet een functie die de volgende kaart uit de string/array pakt
+# func ...
