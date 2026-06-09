d900bcc54eb109e60f8b623070dc68c475f58836
15838234 <kevin.bergsma@student.uva.nl>
2026-06-09 22:00:12 +0200

    WORKING SCENE SWITCHING SCRIPT

---
+++ frontend/Scripts/multi_lobby.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@export var game_scene: StringName = &""
+@export var start_lobby_button: Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_lobby_button.pressed.connect(_on_start_lobby)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_start_lobby() -> void:
+	SceneLoader.load_scene(game_scene)
