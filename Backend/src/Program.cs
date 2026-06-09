5d6b2fdc3afb02510966398b8e9c004965700e2b
Jayden <jayden_gunhan@hotmail.com>
2026-06-10 00:28:59 +0200

    Works now heh

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -48,7 +48,7 @@ app.Map("/lobby", async (HttpContext context, ConnectionManager connectionManage
 
         if (!connectionManager.IsLobbyAvailable(lobbyId))
         {
-            Console.WriteLine($"Rejected {playerId}: Lobby {lobbyId} is full or doesn't exist. wow take 7");
+            Console.WriteLine($"Rejected {playerId}: Lobby {lobbyId} is full or doesn't exist.");
             context.Response.StatusCode = StatusCodes.Status403Forbidden;
             return;
         }
