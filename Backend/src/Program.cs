80f72525f85f15b50a1b7a835e3a01f489138de3
Jayden <jayden_gunhan@hotmail.com>
2026-06-10 00:10:29 +0200

    take 5 lol

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -48,7 +48,7 @@ app.Map("/lobby", async (HttpContext context, ConnectionManager connectionManage
 
         if (!connectionManager.IsLobbyAvailable(lobbyId))
         {
-            Console.WriteLine($"Rejected {playerId}: Lobby {lobbyId} is full or doesn't exist. wow take 4");
+            Console.WriteLine($"Rejected {playerId}: Lobby {lobbyId} is full or doesn't exist. wow take 5");
             context.Response.StatusCode = StatusCodes.Status403Forbidden;
             return;
         }
