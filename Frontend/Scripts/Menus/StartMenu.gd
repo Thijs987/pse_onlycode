4ed3d72336bfcd40a8357fae39adb9f0a0096a19
15838234 <kevin.bergsma@student.uva.nl>
2026-06-18 01:48:13 +0200

    look good

--- Frontend/Scripts/Menus/StartMenu.gd
+++ Frontend/Scripts/Menus/StartMenu.gd
@@ -7,8 +7,14 @@ extends Control
 @onready var background: TextureRect = $Background
 
 @export var multi_lobby: StringName = &""
+@export var main_menu_buttons: MarginContainer
+@export var first_menu_screen: MarginContainer
+@export var press_to_start: TextureRect
+@export var press_start_animation: AnimationPlayer
+@export var movement_ease_curve: Curve
 
 var bg_start_pos
+var in_main_menu: bool = false
 
 # TEMP FOR TESTING
 var is_logged_in: bool = false
@@ -16,6 +22,7 @@ var is_muted: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	press_start_animation.play("PressStartAnimation")
 	bg_start_pos = background.position
 	
 	login_button.pressed.connect(_on_login_pressed)
@@ -26,7 +33,26 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_background()
+	
+func _input(event: InputEvent) -> void:
+	if !in_main_menu and event.is_pressed():
+			press_start_animation.play("PressStartPressed")
+			await press_start_animation.animation_finished
+			main_menu_buttons.visible = true
+			show_buttons_menu(first_menu_screen)
+			in_main_menu = true
 
+func show_buttons_menu(TitleContainer):
+	var tween = create_tween()
+	var end_pos = TitleContainer.position + Vector2(0, -80)
+	
+	tween.tween_method(
+		func(progress):
+			var curve_value = movement_ease_curve.sample(progress)
+			TitleContainer.position = lerp(TitleContainer.position, end_pos, curve_value), 0.0, 1.0, 4
+	)
+
+# Creates the moving background
 func move_background() -> void:
 	background.position.x -= 0.25
 	background.position.y -= 0.5
