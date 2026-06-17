0994037ef614b03cf97700b7419e76483db232d2
Thijs Burger <thijsburger2006@gmail.com>
2026-06-17 10:45:50 +0200

    It had to be a bool instead of player_id

--- Frontend/Scripts/Cards/CardLogic.gd
+++ Frontend/Scripts/Cards/CardLogic.gd
@@ -45,7 +45,7 @@ func play_card(card):
 		print("Error: DiscardPileArea node not found!")
 		return
 
-	if discard_area.overlaps_area(card.get_node("Area2D")) and controller.PId == turn:
+	if discard_area.overlaps_area(card.get_node("Area2D")) and turn == true:
 		card.movable = false
 
 		highlight_card(card, false)
