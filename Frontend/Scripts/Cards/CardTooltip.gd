04121f2e8690486d2c6cca9bb4a9b06949d4b73e
Thijs Burger <thijsburger2006@gmail.com>
2026-06-18 19:39:33 +0200

    Changed tooltip texts and fixed panel size not updating correctly

--- Frontend/Scripts/Cards/CardTooltip.gd
+++ Frontend/Scripts/Cards/CardTooltip.gd
@@ -5,6 +5,7 @@ extends Control
 
 func show_tooltip(card_id):
 	tooltip_text(card_id)
+	label.reset_size()
 	panel.z_index = 1000
 	panel.size = label.size
 	panel.show()
@@ -15,32 +16,32 @@ func hide_tooltip():
 func tooltip_text(card_id):
 	match card_id:
 		"blue":
-			label.text = "You can't play this card"
+			label.text = "This card is unplayable\nLose it by using cards their effects"
 		"cm":
-			label.text = "Skip drawing a card"
+			label.text = "End your turn without drawing a card"
 		"ddos":
-			label.text = "Next player plays 2 turns\nYou skip drawing a card"
+			label.text = "End your turn without drawing a card\nNext player has to take two turns"
 		"err":
-			label.text = "You can't play this card"
+			label.text = "This card is unplayable\nLose it by using cards their effects"
 		"goto":
-			label.text = "Play 2 of this card to give a player the top card of the drawing pile\nCan also be used as a joker with the other bad coding habits"
+			label.text = "Give the top card of the drawing pile to a player\nYou can use this card as a joker for any other bad coding habit"
 		"inf":
-			label.text = "Play 2 of this card to give a player the top card of the drawing pile"
+			label.text = "Give the top card of the drawing pile to a player\nYou need 2 of this card to play them"
 		"merge":
-			label.text = "You can't play this card"
+			label.text = "This card is unplayable\nLose it by using cards their effects"
 		"miracle":
-			label.text = "Shuffle the drawing pile"
+			label.text = "Shuffle the cards in the drawing pile"
 		"nocom":
-			label.text = "Play 2 of this card to give a player the top card of the drawing pile"
+			label.text = "Give the top card of the drawing pile to a player\nYou need 2 of this card to play them"
 		"os":
-			label.text = "Draw and see the bottom card of the draw pile\nDecide whether to keep the card or put it on top"
+			label.text = "Draw and look at the bottom card of the drawing pile\nDecide wether to keep it or put it on top of the drawing pile"
 		"sql":
-			label.text = "Decide which player plays 2 turns\nYou skip drawing a card"
+			label.text = "End your turn without drawing a card\nDecide which player has to take two turns"
 		"test":
 			label.text = "Oops, this card shouldn't be here\nThis card is for tests!"
 		"trojan":
-			label.text = "Give a card in your hand to another player"
+			label.text = "Choose a player to give a card from your hand"
 		"vibe":
-			label.text = "Play 2 of this card to give a player the top card of the drawing pile"
+			label.text = "Give the top card of the drawing pile to a player\nYou need 2 of this card to play them"
 		_:
-			label.text = "Mystery card: text can't be loaded"
+			label.text = "Error: this card should not exist"
