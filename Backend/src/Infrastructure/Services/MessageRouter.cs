e51d5d548564bc5ea25c3ae5a334d2cf09d9aaad
Egg-ehe <noah.eggink@student.uva.nl>
2026-06-26 00:20:00 +0200

    Fixed imp not showing on discard pile

--- Backend/src/Infrastructure/Services/MessageRouter.cs
+++ Backend/src/Infrastructure/Services/MessageRouter.cs
@@ -153,6 +153,7 @@ public class MessageRouter
                         response = MakeMessage(action, playerId, responseData);
                         if (responseData.CardId == "imp" && !matchManager.GetPlayerHand(lobbyId, playerId).Contains("imp"))
                         {
+                            await connectionManager.BroadcastToLobbyAsync(lobbyId, SerializeMsg(response));
                             await Next_player(lobbyId, playerId, false, connectionManager, matchManager);
                         }
                         else if (responseData.CardId == "imp")
@@ -200,7 +201,7 @@ public class MessageRouter
                             await connectionManager.BroadcastToLobbyAsync(lobbyId, SerializeMsg(broadMessage), responseData.Target);
                             await connectionManager.SendMessageAsync(responseData.Target, SerializeMsg(response));
                         }
-                        else if (responseData.IsPrivate == false)
+                        else if (responseData.IsPrivate == false && !(responseData.CardId == "imp" && !matchManager.GetPlayerHand(lobbyId, playerId).Contains("imp")))
                         {
                             await connectionManager.BroadcastToLobbyAsync(lobbyId, SerializeMsg(response));
                         }
